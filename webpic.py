e8f3268c8771312f10ce10a18a2cbfab1dfd1c6f
Rafail Abdyushev <rafaelab@mail.ru>
2023-10-04 16:31:31 +0300

    app corrected methods names

--- webpic.py
+++ webpic.py
@@ -5,7 +5,7 @@ import aiohttp
 from urllib.parse import urlparse
 import os
 
-async def fetchAsync(url):
+async def parseURL(url):
     img = []
     # http = urlparse(url)
     http = '{uri.scheme}://{uri.hostname}'.format(uri=urlparse(url))
@@ -19,13 +19,12 @@ async def fetchAsync(url):
                 decoded_string = text.decode("utf-8", errors='replace')
                 img = pat.findall(decoded_string)
                 print(img)
-                # img = list(filter(lambda x: x.startswith('http'), pat.findall(decoded_string)))
+
     img1 = list(map(lambda x: x if x.startswith('http') else http + x, img))
     return img1
 
 async def picFile(dirName, url):
     pr = urlparse(url)
-    # print(pr.path)
     fileName = os.path.join(dirName, os.path.basename(pr.path))
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
@@ -34,27 +33,26 @@ async def picFile(dirName, url):
                     async for chunk in response.content.iter_chunked(10):
                         fd.write(chunk)
 
-async def arrayOfFiles(dirName, urls):
+async def arrayOfPicURLs(dirName, urls):
     tasks = []
     for url in urls:
         task = asyncio.ensure_future(picFile(dirName, url))
         tasks.append(task)
     await asyncio.gather(*tasks)
 
-async def startAsync(dirName, url):
-    task1 = asyncio.create_task(fetchAsync(url))
+async def startPArseUrlAndPics(dirName, url):
+    task1 = asyncio.create_task(parseURL(url))
     urlPics = await task1
-    # http = urlparse(url)
-    task2 = asyncio.create_task(arrayOfFiles(dirName, urlPics))
+    task2 = asyncio.create_task(arrayOfPicURLs(dirName, urlPics))
     await task2
 
-async def startAsyncS(urls):
+async def startAsync(urls):
     tasks = []
     for url in urls:
         pr = urlparse(url)
         dirName = os.path.join(os.getcwd(), pr.hostname)
         os.mkdir(dirName)
-        task = asyncio.ensure_future(startAsync(dirName, url))
+        task = asyncio.ensure_future(startPArseUrlAndPics(dirName, url))
         tasks.append(task)
     await asyncio.gather(*tasks)
     
@@ -62,5 +60,5 @@ if __name__ == '__main__':
     urls = ['https://gb.ru/', 'https://habr.com/ru/all/', 'https://www.google.ru/']
     # urls = ['https://gb.ru/']
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(startAsyncS(urls))
+    loop.run_until_complete(startAsync(urls))
     # asyncio.run(startAsyncS(urls))

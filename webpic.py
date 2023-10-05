4610a15158f95f317b8ecd198168d3da53f2d29a
Rafail Abdyushev <rafaelab@mail.ru>
2023-10-05 11:03:03 +0300

    sys args

--- webpic.py
+++ webpic.py
@@ -1,9 +1,8 @@
-import requests
 import re
 import asyncio
 import aiohttp
 from urllib.parse import urlparse
-import os
+import sys, os, shutil, time
 
 async def parseURL(url):
     img = []
@@ -18,7 +17,7 @@ async def parseURL(url):
                     text += data
                 decoded_string = text.decode("utf-8", errors='replace')
                 img = pat.findall(decoded_string)
-                print(img)
+                # print(img)
 
     img1 = list(map(lambda x: x if x.startswith('http') else http + x, img))
     return img1
@@ -33,17 +32,19 @@ async def picFile(dirName, url):
                     async for chunk in response.content.iter_chunked(10):
                         fd.write(chunk)
 
-async def arrayOfPicURLs(dirName, urls):
+async def arrayOfPicURLs(dirName, urls, start_time):
     tasks = []
     for url in urls:
         task = asyncio.ensure_future(picFile(dirName, url))
         tasks.append(task)
     await asyncio.gather(*tasks)
+    print(f"Downloaded {dirName} in {time.time() - start_time:.2f} seconds")
 
 async def startPArseUrlAndPics(dirName, url):
+    start_time = time.time()
     task1 = asyncio.create_task(parseURL(url))
     urlPics = await task1
-    task2 = asyncio.create_task(arrayOfPicURLs(dirName, urlPics))
+    task2 = asyncio.create_task(arrayOfPicURLs(dirName, urlPics, start_time))
     await task2
 
 async def startAsync(urls):
@@ -51,14 +52,19 @@ async def startAsync(urls):
     for url in urls:
         pr = urlparse(url)
         dirName = os.path.join(os.getcwd(), pr.hostname)
-        os.mkdir(dirName)
+        if os.path.exists(dirName):
+            shutil.rmtree(dirName)
+            os.mkdir(dirName)
+        else:
+            os.mkdir(dirName)
         task = asyncio.ensure_future(startPArseUrlAndPics(dirName, url))
         tasks.append(task)
     await asyncio.gather(*tasks)
     
 if __name__ == '__main__':
-    urls = ['https://gb.ru/', 'https://habr.com/ru/all/', 'https://www.google.ru/']
-    # urls = ['https://gb.ru/']
+    initialData = sys.argv
+    defaultData = ['https://gb.ru/', 'https://habr.com/ru/all/', 'https://www.google.ru/']
+    urls = initialData[1:] if len(initialData) > 1 else defaultData
     loop = asyncio.get_event_loop()
     loop.run_until_complete(startAsync(urls))
-    # asyncio.run(startAsyncS(urls))
+    # asyncio.run(startAsync(urls))
